c34b206a9d0ef40d06f52f5bea5b9f88cc5817ce
vcedgar <vatriciaedgar@gmail.com>
2017-03-22 19:51:29 -0700

    commenting

--- src/MainFrame/ChessFrame/StatusPanel.java
+++ src/MainFrame/ChessFrame/StatusPanel.java
@@ -9,13 +9,18 @@ import javax.swing.border.LineBorder;
 
 
 public class StatusPanel extends JPanel {
+	//this class is for the box with the black border and red text that appears at the bottm of the screen
+    private JLabel statusLabel=new JLabel();
+    private LineBorder LabelBorder=new LineBorder(Color.BLACK.brighter(),2);
     
     /** Creates a new instance of StatusPanel */
     public StatusPanel() {
+    	//info for the panel itself
         setSize(580,30);
         setLocation(10,610);
         setLayout(null);
         
+        //infor for the label within the panel
         statusLabel.setSize(570,25);
         statusLabel.setLocation(5,5);
         statusLabel.setText(" Start New Game ");
@@ -24,18 +29,13 @@ public class StatusPanel extends JPanel {
         statusLabel.setForeground(Color.RED.brighter());
         statusLabel.setBorder(LabelBorder);
         add(statusLabel);
-        
-        
-        
     }
-    public void start_Again() {
+    
+    public void start_Again() {//called when the game actually begins
         statusLabel.setText("  Game Started ");
     }
     
-    public void changeStatus(Object str) {
+    public void changeStatus(Object str) {//fnc that changes what it says in reference to players turn (and possibly for end game)
         statusLabel.setText((String) str);
     }
-    
-    private JLabel statusLabel=new JLabel();
-    private LineBorder LabelBorder=new LineBorder(Color.BLACK.brighter(),2);
 }
