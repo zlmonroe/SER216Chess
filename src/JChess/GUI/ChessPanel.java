63bee8a72ad88e0729adb5f60dd02f83a1a9edf9
zlmonroe <zachary.lynn.monroe@gmail.com>
2017-04-12 18:32:06 -0700

    Added refactored GUI code

---
+++ src/JChess/GUI/ChessPanel.java
@@ -0,0 +1,54 @@
+package JChess.GUI;
+
+import javax.swing.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.*;
+
+import JChess.Game.Player;
+import JChess.Game.Pieces.*;
+
+
+/**
+ * Created by zachary on 4/11/2017.
+ */
+public class ChessPanel extends JPanel {
+    private Rectangle2D board;
+    private final int boardSize = 600;
+    Player P1, P2;
+
+    ChessPanel() {
+        P1 = new Player(P2, true);
+        P2 = new Player(P1, false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if ((i+j)%2==1) {
+                    g2.setColor(Color.BLUE);
+                    g2.fillRect(i * (boardSize / 8), j * (boardSize / 8), boardSize / 8, boardSize / 8);
+                }
+                else {
+                    g2.setColor(Color.WHITE);
+                    g2.fillRect(i * (boardSize / 8), j * (boardSize / 8), boardSize / 8, boardSize / 8);
+                }
+            }
+        }
+
+        drawPieces(P1, g2);
+        drawPieces(P2, g2);
+    }
+
+    public void drawPieces(Player player, Graphics2D g2) {
+        for (Piece p : player.pieces) {
+            p.setPixels(boardSize / 8 * p.returnX(), boardSize / 8 * p.returnY());
+            int x = p.getPixelX(), y = p.getPixelY();
+            Image img = p.returnPieceImage();
+            g2.drawImage(img, x + boardSize / 40, y, boardSize / 13, boardSize / 10, this);
+        }
+    }
+}
