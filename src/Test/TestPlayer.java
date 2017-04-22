2b40a2f6e24d826165f5ab77c11d4cf19724d6d4
vcedgar <vatriciaedgar@gmail.com>
2017-04-22 14:06:33 -0700

    Create fnc for making mocks

--- src/Test/TestPlayer.java
+++ src/Test/TestPlayer.java
@@ -17,26 +17,51 @@ import Game.*;
 
 public class TestPlayer {
 	BoardState mockState1;
+	Piece mockKing;
+	Piece mockQueen;
 	Player player1;
 	@Before
 	public void setUp() throws Exception {
-		mockState1 = mock(BoardState.class);
 		player1 = new Player(false);
 		Player.state = mockState1;
 		Point kingPoint = new Point(1, 7);
 		Point queenPoint = new Point(1, 6);
-		Point bishopPoint = new Point(2, 5);
 		LinkedList<Piece> blackPieces = new LinkedList();
 		blackPieces.add(new King(kingPoint, false));
 		LinkedList<Piece> whitePieces = new LinkedList();
 		whitePieces.add(new Queen(queenPoint, true));
-		whitePieces.add(new Bishop(bishopPoint, true));
 		when(mockState1.getPieceAt(kingPoint)).thenReturn(blackPieces.get(0));
 		when(mockState1.getPieceAt(queenPoint)).thenReturn(whitePieces.get(0));
 		when(mockState1.getPieceAt(bishopPoint)).thenReturn(whitePieces.get(1));
 		when(mockState1.getPieces(false)).thenReturn(blackPieces);
 		when(mockState1.getPieces(true)).thenReturn(blackPieces);
 		
+	}
+	
+	private void createMock1(){
+		
+		mockKing = mock(King.class);
+		mockQueen = mock(Queen.class);
+		
+		Point kingPoint = new Point(1, 7);
+		Point queenPoint = new Point(1, 6);
+		
+		when(mockKing.isWhite()).thenReturn(false);
+		when(mockQueen.isWhite()).thenReturn(true);
+		when(mockKing.getPosition()).thenReturn(kingPoint);
+		when(mockQueen.getPosition()).thenReturn(queenPoint);
+		
+		mockState1 = mock(BoardState.class);
+		
+		LinkedList<Piece> blackPieces = new LinkedList();
+		blackPieces.add(mockKing);
+		LinkedList<Piece> whitePieces = new LinkedList();
+		whitePieces.add(mockQueen);
+		
+		when(mockState1.getPieceAt(kingPoint)).thenReturn(blackPieces.get(0));
+		when(mockState1.getPieceAt(queenPoint)).thenReturn(whitePieces.get(0));
+		when(mockState1.getPieces(false)).thenReturn(blackPieces);
+		when(mockState1.getPieces(true)).thenReturn(blackPieces);
 	}
 
 	@After
