ad6acb0f186688fc7aed0787439eca2624cfd89d
vcedgar <vatriciaedgar@gmail.com>
2017-04-21 20:26:42 -0700

    Changed everything

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,26 +1,66 @@
-package Game;
+package JChess.Game;
 
-import Game.Pieces.Piece;
+import java.awt.Point;
+import java.util.LinkedList;
 
-import java.awt.*;
+import JChess.Game.Pieces.Piece;
 
-/**
- * Created by tjcup on 4/19/2017.
- */
-public class Player {
-    private static BoardState cb;
-    private Iterable<Piece> myPieces;
-    private Player opponent;
-    private boolean isWhite;
-
-    //Constructor here
-
-    public boolean inCheck(){
-        return false;
-    }
-
-    public boolean inCheckMate(){
-        return false;
-    }
+public class MyPlayer {
+	public static ChessboardState state;
+	private MyPlayer enemy;
+	private boolean isWhite;
+	private LinkedList<Piece> piecesAttackingKing;
+	public enum PieceID{
+		PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING;
+	}
+	
+	public boolean inCheck(BoardState tmp){
+		Piece king = getKing();
+		piecesAttackingKing = new LinkedList<Piece>();
+        for (Piece enemyPiece : tmp.getPieces(!isWhite) {
+            if (enemyPiece.canMove(king.getPoint().x,king.getPoint().y)) {
+                piecesAttackingKing.add(enemyPiece);
+            }
+        }
+        return !piecesAttackingKing.isEmpty();
+	}
+	
+	public boolean inCheckMate(){
+		if(!inCheck(state)){
+			return false;
+		}
+		LinkedList<Piece> pieces = state.getPieces(isWhite);
+		LinkedList<Piece> ePieces = state.getPieces(!isWhite);
+		for(Piece ePiece : piecesAttackingKing){
+			for(Piece piece: pieces){
+				if(piece.getMoves().contains(ePiece.getPoint())){
+					return false;
+				}
+			}
+		}
+		
+	}
+	public boolean move(Point start, Point end){
+		Piece piece = state.getPieceAt(start);
+		LinkedList<Point> pieceMoves = piece.getMoves();
+		if(!pieceMoves.contains(end)){
+			return false;
+		}
+		BoardState tmp = state.move(start, end);
+		if(inCheck(tmp)){
+			return false;
+		}
+		state = tmp;
+		piece.setPoint(end);
+		return true;
+	}
+	
+	private int getKing(){
+		for(int i = 0; i < pieces.size(); i++){
+			if(PieceID.KING == pieces.get(i).getID()){
+				return i;
+			}
+		}
+	}
 
 }
