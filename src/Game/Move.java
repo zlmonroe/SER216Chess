9597845ab9502b9bc67dae84766afce201d1439b
JonathanBush <buoywestone@gmail.com>
2017-04-22 12:19:50 -0700

    Finished implementation of Message and Move

--- src/Game/Move.java
+++ src/Game/Move.java
@@ -7,4 +7,14 @@ import java.awt.*;
  */
 public class Move {
     public Point oldPoint,newPoint;
+
+    /**
+     * Represents a move from oldPoint to newPoint
+     * @param oldPoint The starting position
+     * @param newPoint The ending position
+     */
+    public Move(Point oldPoint, Point newPoint) {
+        this.oldPoint = oldPoint;
+        this.newPoint = newPoint;
+    }
 }
