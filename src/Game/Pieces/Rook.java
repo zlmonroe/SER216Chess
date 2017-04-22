221f5fc9bb6aec92dfeee8a5ccc47b207cde3f6e
zlmonroe <zachary.lynn.monroe@gmail.com>
2017-04-21 19:31:35 -0700

    Added refactored GUI code

--- src/Game/Pieces/Rook.java
+++ src/Game/Pieces/Rook.java
@@ -1,113 +1,35 @@
-
-
 package Game.Pieces;
 
-
-import java.awt.Point;
-
+import java.awt.*;
+import java.util.LinkedList;
 
 public class Rook extends Piece {
 
-    public Rook(String NameIcon, int  startX, int startY) {
-        super(NameIcon, startX, startY);
+    public Rook(Point start, boolean isWhite) {
+        super(start, isWhite);
+        this.identifier = 3;
     }
 
+    @Override
+    @SuppressWarnings("Duplicates")
+    public LinkedList<Point> getMoves() {
+        LinkedList<Point> moves = new LinkedList<>();
 
-    public boolean canMove(int x, int y,String s) {
-        return (y==Y &&(x>(X)||(x<(X)))) || (((y>Y)||(y<Y))&&(x==(X)));
-    }
-    public boolean pieceInMyWay(int x, int y, Point othersPosition) {
-        int j=y;
-        int i=x;
-        if(((y==Y)&&(x>(X)||(x<(X))))) {
-            
-            if((X<i))
-                
-                while( (i!=X+1)) {
-                i--;
-                if(((othersPosition.y)==j)&&((othersPosition.x==i)))//there Same Color piece
-                {
-                    return true;
+        int originX = this.position.x;
+        int originY = this.position.y;
+
+        int[][] offsets = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+        for(int[] offset : offsets) {
+            for (int x = originX, y = originY; x < 8 && y < 8; x += offset[0], y += offset[1]) {
+                Point move = new Point(x, y);
+                Piece pieceAtMove = board.getPieceAt(move);
+                if (pieceAtMove == null || pieceAtMove.isWhite != isWhite) {
+                    moves.add(move);
                 }
-                }
-            
-            else  if((X>i)) {
-                while( (i!=X-1)) {
-                    i++;
-                    if(((othersPosition.y)==j)&&((othersPosition.x==i))) {
-                        return true;
-                    }
-                }
-            }
-        } else if((((y>Y)||(y<Y))&&(x==(X)))) {
-            if((Y<j)) {
-                while((j!=Y+1)) {
-                    j--;
-                    if(((othersPosition.y)==j)&&((othersPosition.x==i))) {
-                        return true;
-                    }
-                }
-            } else  if((Y>j)) {
-                while((j!=Y-1)) {
-                    j++;
-                    
-                    if(((othersPosition.y)==j)&&((othersPosition.x==i))) {
-                        return true;
-                    }
-                }
-                
             }
         }
-        return false;
-        
-        
-    }
-    public boolean checkKing(int x, int y,Point othersPosition) {
-        int j=y;
-        int i=x;
-        if(((y==Y)&&(x>(X)||(x<(X))))) {
-            
-            if((X<i))
-                
-                while( (i!=X )) {
-                i--;
-                if(((othersPosition.y)==j)&&((othersPosition.x==i)))//there Same Color piece
-                {
-                    return true;
-                }
-                }
-            
-            else  if((X>i)) {
-                while( (i!=X )) {
-                    i++;
-                    if(((othersPosition.y)==j)&&((othersPosition.x==i))) {
-                        return true;
-                    }
-                }
-            }
-        } else if((((y>Y)||(y<Y))&&(x==(X)))) {
-            if((Y<j)) {
-                while((j!=Y )) {
-                    j--;
-                    if(((othersPosition.y)==j)&&((othersPosition.x==i))) {
-                        return true;
-                    }
-                }
-            } else  if((Y>j)) {
-                while((j!=Y )) {
-                    j++;
-                    
-                    if(((othersPosition.y)==j)&&((othersPosition.x==i))) {
-                        return true;
-                    }
-                }
-                
-            }
-        }
-        return false;
-        
-    }
-    public String Tell_me() {
-        return "Rook= ("+p.x+','+p.y+")";
+
+        return moves;
     }
 }
