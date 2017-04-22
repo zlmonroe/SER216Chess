24fad6584abe0d774987f3690f552beb26892883
vcedgar <vatriciaedgar@gmail.com>
2017-04-22 14:29:22 -0700

    Comments about mocking

--- src/Test/TestPlayer.java
+++ src/Test/TestPlayer.java
@@ -22,46 +22,41 @@ public class TestPlayer {
 	Player player1;
 	@Before
 	public void setUp() throws Exception {
+		createMock1();
+
 		player1 = new Player(false);
 		Player.state = mockState1;
-		Point kingPoint = new Point(1, 7);
-		Point queenPoint = new Point(1, 6);
-		LinkedList<Piece> blackPieces = new LinkedList();
-		blackPieces.add(new King(kingPoint, false));
-		LinkedList<Piece> whitePieces = new LinkedList();
-		whitePieces.add(new Queen(queenPoint, true));
-		when(mockState1.getPieceAt(kingPoint)).thenReturn(blackPieces.get(0));
-		when(mockState1.getPieceAt(queenPoint)).thenReturn(whitePieces.get(0));
-		when(mockState1.getPieceAt(bishopPoint)).thenReturn(whitePieces.get(1));
-		when(mockState1.getPieces(false)).thenReturn(blackPieces);
-		when(mockState1.getPieces(true)).thenReturn(blackPieces);
-		
 	}
 	
-	private void createMock1(){
+	/*This creates the first mock board state
+	 * It creates a mock board with a mock black king 
+	 * at b7 and a mock white queen at b6 
+	 */
+	private void createMock1(){		
+		mockKing = mock(King.class); //creates mock king
+		mockQueen = mock(Queen.class); //creates mock queen
 		
-		mockKing = mock(King.class);
-		mockQueen = mock(Queen.class);
+		Point kingPoint = new Point(1, 7); //this is the point, b7, that the king is at
+		Point queenPoint = new Point(1, 6); //this is the point, b6, that the queen is at
 		
-		Point kingPoint = new Point(1, 7);
-		Point queenPoint = new Point(1, 6);
+		when(mockKing.isWhite()).thenReturn(false); //the king is black
+		when(mockQueen.isWhite()).thenReturn(true); //the queen is white
+		when(mockKing.getPosition()).thenReturn(kingPoint); //the kings position should return a point 1, 7 (b7)
+		when(mockQueen.getPosition()).thenReturn(queenPoint); //the queens position should return a point 1, 6 (b6)
+		when(mockKing.getIdentifier()).thenReturn(5); //the king should return the number that identifies it as a king
+		when(mockQueen.canMove(kingPoint)).thenReturn(true);// the queen has the ability to move forward one, as is chess rules
 		
-		when(mockKing.isWhite()).thenReturn(false);
-		when(mockQueen.isWhite()).thenReturn(true);
-		when(mockKing.getPosition()).thenReturn(kingPoint);
-		when(mockQueen.getPosition()).thenReturn(queenPoint);
+		mockState1 = mock(BoardState.class);//creates mock board
 		
-		mockState1 = mock(BoardState.class);
-		
-		LinkedList<Piece> blackPieces = new LinkedList();
+		LinkedList<Piece> blackPieces = new LinkedList(); //linked list with all of black pieces
 		blackPieces.add(mockKing);
-		LinkedList<Piece> whitePieces = new LinkedList();
+		LinkedList<Piece> whitePieces = new LinkedList(); //linked list with all of white pieces
 		whitePieces.add(mockQueen);
 		
-		when(mockState1.getPieceAt(kingPoint)).thenReturn(blackPieces.get(0));
-		when(mockState1.getPieceAt(queenPoint)).thenReturn(whitePieces.get(0));
-		when(mockState1.getPieces(false)).thenReturn(blackPieces);
-		when(mockState1.getPieces(true)).thenReturn(blackPieces);
+		when(mockState1.getPieceAt(kingPoint)).thenReturn(blackPieces.get(0)); //when we ask for the piece at the king's point, we should get the king
+		when(mockState1.getPieceAt(queenPoint)).thenReturn(whitePieces.get(0)); //when we ask for the piece at the queen's point, we should get the queen
+		when(mockState1.getPieces(false)).thenReturn(blackPieces); //when we ask for black pieces, we should get black pieces
+		when(mockState1.getPieces(true)).thenReturn(whitePieces); //when we ask for white pieces, we should get white pieces
 	}
 
 	@After
