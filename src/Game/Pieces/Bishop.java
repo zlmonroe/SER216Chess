5dfaf70dc10a2b0ea26506add2bf0f5484d5ae37
zlmonroe <zachary.lynn.monroe@gmail.com>
2017-04-21 19:31:35 -0700

    Added refactored GUI code

--- src/Game/Pieces/Bishop.java
+++ src/Game/Pieces/Bishop.java
@@ -16,13 +16,16 @@ public class Bishop extends Piece {
         int originX = this.position.x;
         int originY = this.position.y;
 
-        for(int x = originX, y = originY; x < 8 && y < 8; x++, y++) {
-           Point move = new Point(x,y);
-           Piece pieceAtMove = board.getPieceAt(move);
-           if(pieceAtMove == null || pieceAtMove.isWhite != isWhite ){
-               moves.add(move);
-            }
+        int[][] offsets = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
 
+        for(int[] offset : offsets) {
+            for (int x = originX, y = originY; x < 8 && y < 8; x += offset[0], y += offset[1]) {
+                Point move = new Point(x, y);
+                Piece pieceAtMove = board.getPieceAt(move);
+                if (pieceAtMove == null || pieceAtMove.isWhite != isWhite) {
+                    moves.add(move);
+                }
+            }
         }
 
         return moves;
--- src/Game/Pieces/Queen.java
+++ src/Game/Pieces/Queen.java
@@ -1,168 +1,25 @@
 
 package Game.Pieces;
 import java.awt.Point;
+import java.util.LinkedList;
+
 public class Queen extends Piece {
-    public Queen(String NameIcon, int startX, int startY) {
-        super(NameIcon, startX, startY);
+    private Bishop bishop;
+    private Rook rook;
+
+    public Queen(Point start, boolean isWhite) {
+        super(start, isWhite);
+        bishop = new Bishop(start, isWhite);
+        rook = new Rook(start, isWhite);
+
+        this.identifier = 4;
     }
-    public boolean canMove(int x, int y, String s) {
-        if (((y == Y) && (x > (X) || (x < X)))) {
-            return true;
-        } else if ((((y > Y) || (y < Y)) && (x == (X)))) {
-            return true;
-        } else if ((x - y) == (X - Y)) {
-            return true;
-        } else return (x + y) == (X + Y);
-    }
-    public boolean PieceInMYway(int x, int y, Point othersPosition) {
-        int j = y;
-        int i = x;
-        if (((y == Y) && (x > (X) || (x < (X))))) {
-            if ((X < i))
-                while ((i != X + 1)) {
-                    i--;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i)))//there Same Color piece
-                    {
-                        return true;
-                    }
-                }
-            else if ((X > i)) {
-                while ((i != X - 1)) {
-                    i++;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            }
-        } else if ((((y > Y) || (y < Y)) && (x == (X)))) {
-            if ((Y < j)) {
-                while ((j != Y + 1)) {
-                    j--;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            } else if ((Y > j)) {
-                while ((j != Y - 1)) {
-                    j++;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            }
-        } else if ((x - y) == (X - Y)) {
-            if (x > X && y > Y) {
-                while ((j != Y + 1) && (i != X + 1)) {
-                    j--;
-                    i--;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            } else if (x < X && y < Y)
-                while ((j != Y - 1) && (i != X - 1)) {
-                    j++;
-                    i++;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-        } else if ((x + y) == (X + Y)) {
-            if ((X < i) && (Y > j)) {
-                while ((j != Y - 1) && (i != X + 1)) {
-                    j++;
-                    i--;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            } else if ((X > i) && (Y < j)) {
-                while ((j != Y + 1) && (i != X - 1)) {
-                    j--;
-                    i++;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-    public boolean checkKing(int x, int y, Point othersPosition) {
-        int j = y;
-        int i = x;
-        if (((y == Y) && (x > (X) || (x < (X))))) {
-            if ((X < i))
-                while ((i != X)) {
-                    i--;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i)))//there Same Color piece
-                    {
-                        return true;
-                    }
-                }
-            else if ((X > i)) {
-                while ((i != X)) {
-                    i++;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            }
-        } else if ((((y > Y) || (y < Y)) && (x == (X)))) {
-            if ((Y < j)) {
-                while ((j != Y)) {
-                    j--;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            } else if ((Y > j)) {
-                while ((j != Y)) {
-                    j++;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            }
-        } else if ((x - y) == (X - Y)) {
-            if (x > X && y > Y) {
-                while ((j != Y) && (i != X)) {
-                    j--;
-                    i--;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            } else if (x < X && y < Y)
-                while ((j != Y) && (i != X)) {
-                    j++;
-                    i++;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-        } else if ((x + y) == (X + Y)) {
-            if ((X < i) && (Y > j)) {
-                while ((j != Y) && (i != X)) {
-                    j++;
-                    i--;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            } else if ((X > i) && (Y < j)) {
-                while ((j != Y) && (i != X)) {
-                    j--;
-                    i++;
-                    if (((othersPosition.y) == j) && ((othersPosition.x == i))) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-    public String Tell_me() {
-        return "Queen= (" + p.x + ',' + p.y + ")";
+
+    @Override
+    public LinkedList<Point> getMoves() {
+        LinkedList<Point> movesB = bishop.getMoves();
+        LinkedList<Point> movesR = rook.getMoves();
+        movesB.addAll(movesR);
+        return movesB;
     }
 }
