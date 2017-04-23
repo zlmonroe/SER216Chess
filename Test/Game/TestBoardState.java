3d206dcc615d7e09bc07ce4ba182a6525df2a2f2
JonathanBush <buoywestone@gmail.com>
2017-04-22 20:01:12 -0700

    Test suite working with JUnite 4.11, Hamcrest 1.3

--- Test/Game/TestBoardState.java
+++ Test/Game/TestBoardState.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertNotEquals;
 /**
  * Created by jon on 4/22/2017.
  */
-public class BoardStateTest {
+public class TestBoardState {
     BoardState bs;
 
     @Before
