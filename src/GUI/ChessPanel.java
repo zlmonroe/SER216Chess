2ef68285ed5ab5d588b4d8d4f8fc41fa902df39a
zlmonroe <zachary.lynn.monroe@gmail.com>
2017-04-24 20:13:55 -0700

    fixed pawn clone

--- src/GUI/ChessPanel.java
+++ src/GUI/ChessPanel.java
@@ -61,7 +61,5 @@ public class ChessPanel extends JPanel {
                 }
             }
         }
-
-
     }
 }
--- src/Game/GameServer.java
+++ src/Game/GameServer.java
@@ -1,5 +1,6 @@
 package Game;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
@@ -13,20 +14,21 @@ public class GameServer {
     private ObjectInputStream whiteIn;
     private ObjectOutputStream blackOut;
     private ObjectInputStream blackIn;
+    Player whitePlayer, blackPlayer;
 
-    public static void main(String[] args){
-        if(args.length>0){
+    public static void main(String[] args) {
+        if (args.length > 0) {
             new GameServer(Integer.parseInt(args[0]));
-        }
-        else
+        } else
             new GameServer(5000);
     }
-    public GameServer(int portNum){
+
+    public GameServer(int portNum) {
         boolean whiteConnected = false;
         boolean blackConnected = false;
-        Player whitePlayer = new Player(true);
-        Player blackPlayer = new Player(false);
-        try{
+        whitePlayer = new Player(true);
+        blackPlayer = new Player(false);
+        try {
             ServerSocket serverSocket = new ServerSocket(portNum);
             Socket whiteSocket = serverSocket.accept();
             whiteConnected = true;
@@ -34,44 +36,131 @@ public class GameServer {
             blackConnected = true;
             System.out.println("All Connected");
 
-            whiteOut =  new ObjectOutputStream(whiteSocket.getOutputStream());
+            whiteOut = new ObjectOutputStream(whiteSocket.getOutputStream());
             whiteIn = new ObjectInputStream(whiteSocket.getInputStream());
 
             blackOut = new ObjectOutputStream(blackSocket.getOutputStream());
-            blackIn =  new ObjectInputStream(blackSocket.getInputStream());
-            whiteOut.writeObject(new Message(true,null,null,null,0));
-            blackOut.writeObject(new Message(false,null,null,null,0));
+            blackIn = new ObjectInputStream(blackSocket.getInputStream());
+            whiteOut.writeObject(new Message(true, null, null, null, 0));
+            blackOut.writeObject(new Message(false, null, null, null, 0));
             blackSocket.setSoTimeout(500);
             whiteSocket.setSoTimeout(500);
-            boolean turn = true;
-
+        } catch (Throwable e) {
         }
-        catch (Throwable e) {
-
-        }
-        while(whiteConnected && blackConnected) {
-            Message whiteMessage;
-            Message blackMessage;
+        boolean isWhiteTurn = true;
+        State gameState = State.PROGRESS;
+        while(whiteConnected && blackConnected && gameState == State.PROGRESS) {
+            Message whiteMessage = null;
+            Message blackMessage = null;
+            boolean newInfo = false;
 
             try {
                 whiteMessage = (Message) whiteIn.readObject();
-                System.out.println("White sent: " + whiteMessage.newMessage);
-                blackOut.writeObject(whiteMessage);
-                blackOut.reset();
             } catch (Exception e) {
             }
 
             try {
                 blackMessage = (Message) blackIn.readObject();
-                System.out.println("Black sent: " + blackMessage.newMessage);
-                whiteOut.writeObject(blackMessage);
-                whiteOut.reset();
             } catch (Exception e) {
             }
 
+            try {
+                if (whiteMessage != null) {
+                    if (processMove(whiteMessage)) {
+                        isWhiteTurn = !isWhiteTurn;
+                        gameState = checkGameState(isWhiteTurn);
+                    }
+                }
+                if (blackMessage != null) {
+                    if (processMove(blackMessage)) {
+                        isWhiteTurn = !isWhiteTurn;
+                        gameState = checkGameState(isWhiteTurn);
+                    }
+                }
+            }
+            catch (IOException e) {
+                System.err.println("The move could not be processed (likely due to a connection error)!");
+            }
+        }
 
+    }
+
+    boolean processMove(Message message) throws IOException {
+        if (message.newMessage != null) {
+            //move is just a message
+            if (message.isWhite) {
+                //if the message is from white
+                blackOut.writeObject(message);
+                blackOut.reset();
+            } else {
+                whiteOut.writeObject(message);
+                whiteOut.reset();
+            }
+        } else {
+            //move contains info to be processed
+            Move move = message.newMove;
+            if (message.isWhite) {
+                if (whitePlayer.move(move.oldPoint, move.newPoint)) {
+                    if(blackPlayer.inCheck()) {
+                        //the move has happened and the black player is now in check
+                        message = message.setNewGameInfo("Black is now in check!");
+                    }
+                    informMoveUpdate(message);
+                    return true;
+                }
+            } else {
+                if (blackPlayer.move(move.oldPoint, move.newPoint)) {
+                    if(whitePlayer.inCheck()) {
+                        //the move has happened and the white player is now in check
+                        message = message.setNewGameInfo("White is now in check!");
+                    }
+                    informMoveUpdate(message);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private enum State {
+        STALEMATE   (0b000),
+        PROGRESS    (0b001),
+        WHITELOSES  (0b110),
+        BLACKLOSES  (0b010),
+        ERROR       (0b011);
+
+        public int val;
+
+        State(int val) {
+            this.val = val;
         }
     }
 
+    private State checkGameState(boolean turn) {
+        int player = turn ? 1:0;
+        int checkMate, hasMoves;
+        if(turn) {
+            checkMate = whitePlayer.inCheckMate() ? 1 : 0;
+            hasMoves = whitePlayer.hasMoves() ? 1:0;
+        }
+        else {
+            checkMate = blackPlayer.inCheckMate() ? 1 : 0;
+            hasMoves = blackPlayer.hasMoves() ? 1:0;
+        }
 
+        return State.values()[player << 2 & checkMate << 1 << hasMoves];
+    }
+
+    private void informMoveUpdate(Message message) {
+        try {
+            blackOut.writeObject(message);
+            blackOut.reset();
+            whiteOut.writeObject(message);
+            whiteOut.reset();
+        }
+        catch (IOException e) {
+            System.err.println("Could not inform players (likely due to a connection error)");
+        }
+
+    }
 }
